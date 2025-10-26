e8de92a424fc9cd53617f07a5b51ff70e27dda84
othartzell <ohartzel@uncc.edu>
2025-10-26 15:19:35 -0400

    Added NIST test vectors
    Added the NIST test vectors for AES 128, 192, and 256 to test AES encryption function.

--- AES.py
+++ AES.py
@@ -3,8 +3,9 @@
 # ITIS 6200 Course Project: Implementing AES-CBC Mode encryption from scratch
 
 # AES encryption scheme from scratch
-def aes_encryption(plaintext, key):
-    pass
+def aes_encryption(data: bytes, key: bytes) -> bytes:
+
+    return b''
 
 
 if __name__ == "__main__":
@@ -14,3 +15,21 @@ if __name__ == "__main__":
     key = bytearray.fromhex('000102030405060708090a0b0c0d0e0f')
     expected_ciphertext = bytearray.fromhex('69c4e0d86a7b0430d8cdb78070b4c55a')
     ciphertext = aes_encryption(plaintext, key)
+
+    assert (ciphertext == expected_ciphertext)
+
+    # NIST AES-192 test vector C.2 (p.38)
+    plaintext = bytearray.fromhex('00112233445566778899aabbccddeeff')
+    key = bytearray.fromhex('000102030405060708090a0b0c0d0e0f1011121314151617')
+    expected_ciphertext = bytearray.fromhex('dda97ca4864cdfe06eaf70a0ec0d7191')
+    ciphertext = aes_encryption(plaintext, key)
+
+    assert (ciphertext == expected_ciphertext)
+
+    # NIST AES-256 test vector 3 (Ch. C.3, p. 42)
+    plaintext = bytearray.fromhex('00112233445566778899aabbccddeeff')
+    key = bytearray.fromhex('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f')
+    expected_ciphertext = bytearray.fromhex('8ea2b7ca516745bfeafc49904b496089')
+    ciphertext = aes_encryption(plaintext, key)
+
+    assert (ciphertext == expected_ciphertext)
